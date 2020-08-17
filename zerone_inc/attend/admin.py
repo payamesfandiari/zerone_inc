de6f0d3a26bd3b610a3f5251e8a2d25f497a8633
payamesfandiari <p.esfandiari@samanpr.net>
2020-08-17 11:24:01 +0430

    Add attendance to Admin page

--- zerone_inc/attend/admin.py
+++ zerone_inc/attend/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import Attendance
+
+
+@admin.register(Attendance)
+class OkrAdmin(admin.ModelAdmin):
+    list_display = ["user"]
+    search_fields = ["user"]
+    list_filter = ["user"]
